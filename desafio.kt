64cafa031362854ff838a65640cf75d278f31be0
jessika <jessikafujimura@gmail.com>
2024-01-02 23:09:19 -0300

    inclusão de opçao de selecionar qual formação se matricular

--- desafio.kt
+++ desafio.kt
@@ -1,4 +1,3 @@
-import java.lang.IllegalArgumentException
 import kotlin.random.Random
 
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
@@ -16,16 +15,16 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, 
     fun matricular(usuario: Usuario) {
         if(inscritos.all { it.identificador != usuario.identificador }){
             inscritos.add(usuario);
-            println("${usuario.nome} matriculado com sucesso!")
+            println("${usuario.nome} matriculado(a) na formação $nome com sucesso!")
         } else {
-            throw IllegalArgumentException("${usuario.nome} já está matriculado!")
+            throw IllegalArgumentException("${usuario.nome} já está matriculado(a)!")
         }
     }
 
     fun desmatricular(usuario: Usuario){
         if(inscritos.all { it.identificador == usuario.identificador }){
             inscritos.remove(usuario);
-            println("${usuario.nome} desmatriculado com sucesso!")
+            println("${usuario.nome} desmatriculado(a) com sucesso!")
         }
     }
 
@@ -34,10 +33,24 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, 
 fun main() {
     print("Digite seu nome: ")
     val username = readlnOrNull().toString();
-    val user1 = Usuario( username, Random.nextInt(0, 100))
-    val contents : MutableList<ConteudoEducacional> = mutableListOf(ConteudoEducacional("Git", 30),
-        ConteudoEducacional("Kotlin", 90))
-    val formacao = Formacao("Programação em Kotlin", contents, Nivel.BASICO )
-    formacao.matricular(user1)
+    val user = Usuario( username, Random.nextInt(0, 100))
 
+    val contentKotlin : MutableList<ConteudoEducacional> = mutableListOf(ConteudoEducacional("Git", 30),
+        ConteudoEducacional("Kotlin", 90))
+    val kotlin  = Formacao("Programação em Kotlin", contentKotlin, Nivel.BASICO )
+
+    val contentJava : MutableList<ConteudoEducacional> = mutableListOf(ConteudoEducacional("Git", 30),
+        ConteudoEducacional("Orientação a objeto", 60),ConteudoEducacional("Java", 90))
+    val java  = Formacao("Programação em Java", contentJava, Nivel.INTERMEDIARIO )
+
+    println("Lista de formações disponíveis:  ")
+    println("1 - ${kotlin.nome}")
+    println("2 - ${java.nome}")
+    print("Informe o número de qual formação você deseja se inscrever: ")
+
+    val option = readlnOrNull()?.toInt();
+    when (option){
+        1 -> kotlin.matricular(user)
+        2 -> java.matricular(user)
+    }
 }
