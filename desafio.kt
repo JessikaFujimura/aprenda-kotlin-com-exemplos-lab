a082bf3075ec352c981be0bbd71f9d59b88ebb8f
jessika <jessikafujimura@gmail.com>
2024-01-02 22:38:12 -0300

    resolução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,43 @@
+import java.lang.IllegalArgumentException
+import kotlin.random.Random
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(var nome:String, var identificador: Int)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, var nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if(inscritos.all { it.identificador != usuario.identificador }){
+            inscritos.add(usuario);
+            println("${usuario.nome} matriculado com sucesso!")
+        } else {
+            throw IllegalArgumentException("${usuario.nome} já está matriculado!")
+        }
     }
+
+    fun desmatricular(usuario: Usuario){
+        if(inscritos.all { it.identificador == usuario.identificador }){
+            inscritos.remove(usuario);
+            println("${usuario.nome} desmatriculado com sucesso!")
+        }
+    }
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    print("Digite seu nome: ")
+    val username = readlnOrNull().toString();
+    val user1 = Usuario( username, Random.nextInt(0, 100))
+    val contents : MutableList<ConteudoEducacional> = mutableListOf(ConteudoEducacional("Git", 30),
+        ConteudoEducacional("Kotlin", 90))
+    val formacao = Formacao("Programação em Kotlin", contents, Nivel.BASICO )
+    formacao.matricular(user1)
+
 }
